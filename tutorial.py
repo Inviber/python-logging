026fcc4c7d8c92c459a8ffa112203e00589f8780
Inviber <bkelley4600@gmail.com>
2022-06-07 14:12:41 -0500

    Custom logging

--- tutorial.py
+++ tutorial.py
@@ -2,8 +2,12 @@ import logging
 
 logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w", format="%(asctime)s - %(levelname)s - %(message)s")
 
-try:
-    1 / 0
-except ZeroDivisionError as e:
-    #logging.error("ZeroDivisionError", exc_info=True)
-    logging.exception("ZeroDivisionError")
+logger = logging.getLogger(__name__)
+
+handler = logging.FileHandler('test.log')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+logger.info("test the custom logger")
