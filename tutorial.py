36b380405fae7fc31b9941a6c8b7197e46a8ed39
Inviber <bkelley4600@gmail.com>
2022-06-07 13:59:05 -0500

    Logging with variables

--- tutorial.py
+++ tutorial.py
@@ -1,16 +1,7 @@
 import logging
 
-# Least to most critical
-# logging.debug()
-# logging.info()
-# logging.warning()
-# logging.error()
-# logging.critical()
-
 logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w", format="%(asctime)s - %(levelname)s - %(message)s")
 
-logging.debug("debug")
-logging.info("info")
-logging.warning("warning")
-logging.error("error")
-logging.critical("critical")
+x = 2
+
+logging.info(f"the value of x is {x}")
